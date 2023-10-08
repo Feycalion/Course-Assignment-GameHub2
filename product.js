a95ed5b34246a03919f059bc67d0acf053bfc439
Feycalion <anjakves@gmail.com>
2023-10-08 13:35:26 +0200

    interaction design changes

--- product.js
+++ product.js
@@ -1,17 +1,16 @@
 const id = new URLSearchParams(window.location.search).get("id");
 
-const url = "https://api.noroff.dev/api/v1/gamehub/" + id;
-
 async function getGames() {
+  const url = "https://api.noroff.dev/api/v1/gamehub/" + id;
   const response = await fetch(url);
   const result = await response.json();
 
   productDetails(result);
 }
 
-const gameContainer = document.querySelector(".product-container");
-
 function productDetails(info) {
+  const gameContainer = document.querySelector(".product-container");
+
   const gameTitle = document.getElementById("product-title");
   const gameImage = document.getElementById("product-image");
   const gameDesc = document.getElementById("product-description");
@@ -31,3 +30,72 @@ function productDetails(info) {
 }
 
 getGames();
+
+// Cart indicator
+
+let cartItemCount = localStorage.getItem("cartItemCount")
+  ? parseInt(localStorage.getItem("cartItemCount"))
+  : 0;
+const cartIndicator = document.getElementById("cart-indicator");
+const boxBackground = document.querySelector("rect");
+
+function addToCart() {
+  cartItemCount++;
+  updateCartIndicator();
+}
+
+function updateCartIndicator() {
+  const indicator = document.getElementById("cart-indicator");
+  indicator.textContent = cartItemCount > 0 ? cartItemCount.toString() : "";
+  cartIndicator.style.opacity = cartItemCount > 0 ? 1 : 0;
+  boxBackground.style.opacity = cartItemCount > 0 ? 1 : 0;
+
+  localStorage.setItem("cartItemCount", cartItemCount.toString());
+}
+
+window.onload = () => {
+  updateCartIndicator();
+};
+
+const addToCartButton = document.querySelector(".atcbutton");
+addToCartButton.addEventListener("click", addToCart);
+
+// Toggle favorite games
+
+let heartFilled = localStorage.getItem("heartFilled") === "true";
+
+const fillPath = document.getElementById("heart-fill");
+if (heartFilled) {
+  fillPath.setAttribute("fill", "white");
+} else {
+  fillPath.setAttribute("fill", "none");
+}
+
+function toggleHeartFill() {
+  if (!heartFilled) {
+    fillPath.setAttribute("fill", "white");
+    heartFilled = true;
+  } else {
+    fillPath.setAttribute("fill", "none");
+    heartFilled = false;
+  }
+
+  localStorage.setItem("heartFilled", heartFilled);
+}
+
+/*let heartFilled = false;
+
+
+function toggleHeartFill() {
+  const fillPath = document.getElementById("heart-fill");
+  const outlinePath = document.getElementById("heart-outline");
+
+  if (!heartFilled) {
+    fillPath.setAttribute("fill", "white");
+    heartFilled = true;
+  } else {
+    fillPath.setAttribute("fill", "none");
+    heartFilled = false;
+  }
+}
+*/
